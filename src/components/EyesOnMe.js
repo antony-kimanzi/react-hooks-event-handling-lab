8bbeb657eb1ba569798852255b65cf3252ba8180
antony-kimanzi <antonykimanzi29@gmail.com>
2024-11-14 07:54:22 +0300

    finished lab

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,14 @@
 // Code EyesOnMe Component Here
+// components/EyesOnMe.js
+
+import React from 'react';
+
+function EyesOnMe() {
+return (
+    <button onFocus={() => {console.log('Good!');}} onBlur={() => {console.log('Hey! Eyes on me!');}}>
+      Eyes on me
+    </button>
+  );
+}
+
+export default EyesOnMe;
